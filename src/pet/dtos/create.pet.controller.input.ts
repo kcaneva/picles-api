d71d8ddef8d3f44f2a203f83f18e9020e2750ca2
kcaneva <kleber@caneva.com.br>
2024-04-12 21:19:01 -0300

    Implantação do Swagger e modelo

--- src/pet/dtos/create.pet.controller.input.ts
+++ src/pet/dtos/create.pet.controller.input.ts
@@ -1,24 +1,30 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsString, MaxLength } from "class-validator"
 
 export default class CreatePetControllerInput {
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ description: "Nome do Pet" })
     name: string
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ description: "Tipo do Pet", enum: ['Dog', 'Cat'] })
     type: string
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ description: "Tamanho do Pet", enum: ['Small', 'Medium', 'Large'] })
     size: string
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty({ description: "Genero do Pet", enum: ['Male', 'Female'] })
     gender: string
 
     @IsString()
     @IsNotEmpty()
     @MaxLength(1024)
+    @ApiProperty({ description: "Descrição do Pet", maximum: 1024 })
     bio: string
 }
